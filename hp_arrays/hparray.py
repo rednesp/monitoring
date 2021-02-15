2d3f28515d665e636113f39703a9e4bff7c7ebc6
Antonio Francisco <ajoaoff@gmail.com>
2021-02-15 16:30:07 -0300

    Fix erros in discovery
    Missing 'data' in return from discovery fixed.
    Methods refactored

--- hp_arrays/hparray.py
+++ hp_arrays/hparray.py
@@ -5,14 +5,17 @@ from subprocess import run, PIPE
 import sys
 
 
+def run_hpssacli():
+    """Run the HP application and return a list of lines"""
+    process = run(["/usr/sbin/hpssacli", "ctrl", "all", "show", "config"],
+                  universal_newlines=True, check=True, stdout=PIPE)
+    return process.stdout.split('\n')
+
 def discover(key):
     """Run the HP application and get the 'key' objects (logical or physical)
     drives"""
-    process = run(["/usr/sbin/hpssacli", "ctrl", "all", "show", "config"],
-                  universal_newlines=True, check=True, stdout=PIPE)
-    output = process.stdout.split('\n')
     result = []
-    for line in output:
+    for line in run_hpssacli():
         position = line.find(key)
         if position > -1:
             result.append(line[position:].split()[1])
@@ -20,11 +23,16 @@ def discover(key):
 
 def discover_logical():
     """Discover the logical drives"""
-    return json.dumps([{"{#ARRAY}": value} for value in discover('logicaldrive')])
+    return json.dumps(
+        {'data': [{"{#ARRAY}": value} for value in discover('logicaldrive')]}
+    )
 
 def discover_physical():
     """Discover the physical drives"""
-    return json.dumps([{"{#DISK}": value} for value in discover('physicaldrive')])
+    return json.dumps(
+        {'data': [{"{#DISK}": value} for value in discover('physicaldrive')]}
+    )
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
